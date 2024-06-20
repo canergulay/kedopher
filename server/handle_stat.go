a1e965086baada52b15b4139047b53dc817833bd
canerg <caner.gulay@hotmail.com>
2024-06-20 18:03:25 +0300

    correct statistics endpoint

--- server/handle_stat.go
+++ server/handle_stat.go
@@ -6,6 +6,6 @@ import (
 )
 
 func (ws wsServer) HandleStatistics (w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(fmt.Sprintf("Number of active users: %d \n", len(ws.userHub.Users))))
-	w.Write([]byte(fmt.Sprintf("Number of active connections: %d \n", len(ws.connectionHub.Connections))))
+	w.Write([]byte(fmt.Sprintf("active users: %d \n", len(ws.userHub.Users))))
+	w.Write([]byte(fmt.Sprintf("active connections: %d \n", len(ws.connectionHub.Connections))))
 }

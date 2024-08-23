b92df68c727a271d8f15149576af553efff1c2f8
canerg <caner.gulay@hotmail.com>
2024-08-23 20:50:00 +0300

    add extra logging for connection users array

--- server/handle_candidate.go
+++ server/handle_candidate.go
@@ -7,41 +7,42 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func (w wsServer) handleIceCandidate(message dto.Message,user *model.User){
-	iceCandidate,ok := message.GetBodyAsIceCandidate()
+func (w wsServer) handleIceCandidate(message dto.Message, user *model.User) {
+	iceCandidate, ok := message.GetBodyAsIceCandidate()
 	if !ok {
-		logrus.Errorf("unable to get body as ice candidate for user %s",user.ID)
+		logrus.Errorf("unable to get body as ice candidate for user %s", user.ID)
 		return
 	}
 
 	connection := w.connectionHub.GetConnectionById(model.ID(iceCandidate.ConnectionID))
 	if connection == nil {
-		logrus.Errorf("unable to get connection for user %s",user.ID)
+		logrus.Errorf("unable to get connection for user %s", user.ID)
 		return
 	}
 
 	connection.AddUserToCandidateSentUsers(user.ID)
 	user.SetStatus(enum.UserSentIceCandidate)
 
+	logrus.Infof("connection users in handleIceCandiate: %v", connection.Users)
 
 	// send iceCandidate to all users in the connection except the candidate owner
 	for _, userID := range connection.Users {
-		if userID == user.ID{
+		if userID == user.ID {
 			logrus.Info("userID is same, skipping")
 			continue
 		}
 
 		user := w.userHub.GetUserById(userID)
 		if user == nil {
-			logrus.Warnf("unable to find userID %s",userID)
+			logrus.Warnf("unable to find userID %s", userID)
 			continue
 		}
-	
-		w.sendMessage(user.Conn,dto.Message{
+
+		w.sendMessage(user.Conn, dto.Message{
 			Type: enum.ICE_CANDIDATES,
 			Body: iceCandidate,
 		})
-		logrus.Infof("ice candidates are sent to user %s from user %s",userID,user.ID)
+		logrus.Infof("ice candidates are sent to user %s from user %s", userID, user.ID)
 	}
 
 	w.checkForCandidateSentUsersAndTriggerCall(connection)
@@ -52,18 +53,18 @@ func (w wsServer) checkForCandidateSentUsersAndTriggerCall(connection *model.Con
 		return
 	}
 
-	for _,userID := range connection.CandidateSentUsers {
+	for _, userID := range connection.CandidateSentUsers {
 		user := w.userHub.GetUserById(userID)
 		if user == nil {
-			logrus.Errorf("unable to find user for trigger call %s",userID)
+			logrus.Errorf("unable to find user for trigger call %s", userID)
 			continue
 		}
 
-		w.sendMessage(user.Conn,dto.Message{
+		w.sendMessage(user.Conn, dto.Message{
 			Type: enum.START_CALL,
 			Body: dto.StartCall{
 				ConnectionID: connection.ID,
 			},
 		})
-	} 
+	}
 }
